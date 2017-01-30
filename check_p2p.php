0670088f87634ba246372fdfc23fd1595ccd4f2f
juniorgalhard <silviogalhardojr@gmail.com>
2017-01-30 15:42:59 -0200

    Melhoria no status
    Agora o sistema informa a latencia.

--- check_p2p.php
+++ check_p2p.php
@@ -8,7 +8,7 @@ $username=$argv[3];
 $password=$argv[4];
 $src=$argv[5];
 $dst=$argv[6];
-$repeat="1000";
+$repeat="10";
 
 $ssh = ssh2_connect($host, $port);
   
@@ -26,12 +26,18 @@ $data = '';
 
 while($buffer = fread($stream, 4096)){
 
-	$data .= $buffer;
+        $data .= $buffer;
 }
 
 preg_match('/Success rate is (\d+)/', $data, $match);
 
+preg_match('/round-trip min\/avg\/max = (\d+)\/(\d+)\/(\d+) ms/', $data, $tr);
+
 $packet_sucess=$match[1];
+
+$min=$tr[1];
+$avg=$tr[2];
+$max=$tr[3];
 
 $packet_loss=100-$packet_sucess;
 
@@ -41,15 +47,19 @@ fclose($stream);
 
 switch ($packet_sucess) {
         case "$packet_sucess" ==  "100":
-        print "OK  - $packet_loss % packet loss. | pl=$packet_loss\n";
+        print "OK - $packet_loss % packet loss  min=$min/avg=$avg/max=$max. | pl=$packet_loss;min=$min;avg=$avg;max=$max\n";
         exit(0);
 
-        case "$packet_sucess" < "100":
-        print "WARNING - $packet_loss % packet loss. | pl=$packet_loss\n";
+        case "$packet_sucess" >= "90":
+        print "WARNING - $packet_loss % packet loss  min=$min/avg=$avg/max=$max. | pl=$packet_loss;min=$min;avg=$avg;max=$max\n";
         exit(1);
 
+        case "$packet_sucess" <= "90":
+        print "CRITICAL - $packet_loss % packet loss  min=$min/avg=$avg/max=$max. | pl=$packet_loss;min=$min;avg=$avg;max=$max\n";
+        exit(2);
+
         default:
-        print "UNKNOWN - erro test packet loss.\n";
+        print "UNKNOWN - packet loss error.\n";
         exit(3);
 }
 
